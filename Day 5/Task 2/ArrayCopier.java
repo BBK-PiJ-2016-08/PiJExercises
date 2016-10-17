d32bfb9c4f700086b4b79fbc5c9e4c6a791ac7ab
Jake Holdom <jakeholdombbk@gmail.com>
2016-10-17 16:00:56 +0100

    task 2 completed

---
+++ Day 5/Task 2/ArrayCopier.java
@@ -0,0 +1,56 @@
+class ArrayCopier{
+	//int[] src = new int[4];
+	//int[] dst = new int[4];
+	//int[] result = new int[4];
+	
+	public static void main(String[] args){
+		
+		ArrayCopier AC = new ArrayCopier();
+			
+		AC.begin();
+	}
+	
+	public void begin() {
+		
+		int[] src = {1,2,3,4};
+		int[] dst = new int[4];
+		
+		int[] result = new int[4];
+		result = copy(src, dst);
+		
+		printArray(result);
+		
+		
+	}
+	public int[] copy(int[] src, int[] dst){
+		
+		
+		for (int i= 0; i <= src.length - 1; i++){
+			
+			dst[i] = src[i];
+			
+		}
+		
+		return dst;
+			
+	}
+	
+	private void printArray(int[] array){
+		
+		
+		for (int j = 0; j < array.length; j++){
+			
+			System.out.print(array[j]);
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}

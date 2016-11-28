cea742546a9c519d550e1893d3eb0bc6e0f0f22b
Jake Holdom <jakeholdombbk@gmail.com>
2016-11-28 17:03:26 +0000

    task 5

---
+++ Day 17/Task 5/TaskFive.java
@@ -0,0 +1,12 @@
+public class TaskFive{
+
+
+    public static void main(String[] args){
+
+        TaskFive tsk = new TaskFive();
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+    }
+
+}
